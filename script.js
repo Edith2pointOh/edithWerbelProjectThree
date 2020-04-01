ba9d04433aa61b8de6958e4764d0c9393087c528
Edith Werbel <edith.werbel@gmail.com>
2020-03-31 20:30:48 -0400

    fixed timer

--- script.js
+++ script.js
@@ -1,82 +1,80 @@
 // Global varibles 
 
 let workRound = 1;
-let moveRond = 1;
-let isPaused = true;
-let isWorkActive = false;
+let moveRound = 1;
 
-let workTime = 5;
-let moveTime = 5;
+let workTime = 7;
+let moveTime = 5; 
 let remainingTime = 0;
 let intervalId;
-let DigitalOutput;
+
 
 // const alarm = document.createElement('audio'); alarm.setAttribute("src", "https://www.soundjay.com/misc/sounds/bell-ringing-05.mp3");
 // add rounds to work and move tabs
 $('.work-display').append(" " + workRound);
-$('.move-display').append(" " + moveRond)
+$('.move-display').append(" " + moveRound);
 
 // play button to change to pause on click
-$('.play-btn').on('click', function() {
-    if (workTime == 1500) {
-    countDown25();
-    isWorkSession = true; 
-    } else {
-        // reminder work time to resume
-        if (isWorkSession == true) {
-        remainingTime = workTime;
-        setInterval(showTime, 1000)
-        // reminder move time to resume
-        } else {
-            remainingTime = moveTime;
-            setInterval()
-        }
-    }
 
+$('.play-btn').on('click', function () {
+    countDown25();
     // toggles to pause button
     $('.pause-btn').toggle();
     $('.play-btn').toggle();
 })
 
-$('.pause-btn').on('click', function(){
-    remainingTime = 
-    clearInterval(intervalId);
-    $('.play-btn').toggle();
-    $('.pause-btn').toggle();
-})
+// $('.pause-btn').on('click', function(){
+//     if (isWorkSession == true){
+//         remainingTime = workTime;
+//     clearInterval(intervalId);
+//     $('.play-btn').toggle();
+//     $('.pause-btn').toggle();
+// })
 
 // convert time in seconds to min & sec and display time in digital clock
-function toDigital(){
-    let minutes = Math.floor(workTime / 60);
-    let seconds = Math.floor(workTime % 60);
-    let DigitalOutput = minutes.toString().padStart(2, '0') + ':' +
-    seconds.toString().padStart(2, '0');
-}
-
+// function toDigital(){
+//     let minutes = Math.floor(workTime / 60);
+//     let seconds = Math.floor(workTime % 60);
+//     let digitalOutput = minutes.toString().padStart(2, '0') + ':' +
+//     seconds.toString().padStart(2, '0');
+//     $('.countdown').html(digitalOutput);
+// }
 
 function showTimeWork() {
     workTime--;
-    toDigital();
+    let minutes = Math.floor(workTime / 60);
+    let seconds = Math.floor(workTime % 60);
+    let output = minutes.toString().padStart(2, '0') + ':' +
+        seconds.toString().padStart(2, '0');
     // console.log(output)
+    if (workTime == 0 && workRound == 12){
+        $('.inner-container-work').toggle();
+        $('.inner-container-end-page').toggle();
+    }
     if (workTime == 0) {
         clearInterval(intervalId)
-        // make bell sound
         toggleWorkToMove();
-        countDown5();
+        // make bell sound
     }
-    $('.countdown').html(output);
+    $('.work-countdown').html(output);
+    console.log(output);
 }
 
 function showTimeMove() {
-    moveTime--;
-    
+    moveTime--;  
+    let minutes = Math.floor(moveTime / 60);
+    let seconds = Math.floor(moveTime % 60);
+    let output = minutes.toString().padStart(2, '0') + ':' +
+        seconds.toString().padStart(2, '0');
     // console.log(output)
     if (moveTime == 0) {
         clearInterval(intervalId)
         // make bell sound
         toggleMoveToWork();
     }
-    $('.countdown').html(output);
+    $('.move-countdown').html(output);
+    console.log(moveTime);
+   
 }
 
 function countDown25() {
@@ -85,20 +83,11 @@ function countDown25() {
     // make bell sound
 }
 
-function countDown5() {
+function countdown5() {
     intervalId = setInterval(showTimeMove, 1000);
-    moveRond++;
+    moveRound++;
     // make bell sound
 }
-
-function resumetimer() {
-    intervalId = 
-}
-
-
-// OnLoad, Move Page calls countDown5().
-// An exercise is presented via diagram and short description, chosen at random from array.
-
 
 // to toggle Work and Move pages to load over each other
 
@@ -106,21 +95,24 @@ function toggleWorkToMove() {
     $('.inner-container-move').toggle();
     $('.inner-container-work').toggle();
     $('.inner-container-work').replaceWith($('.inner-container-move'));
-}
+    countdown5();
+    }
+
 
 function toggleMoveToWork() {
     $('inner-container-work').toggle();
     $('inner-container-move').toggle();
     $('inner-container-move').replaceWith($('.inner-container-work'));
+    countDown25();
 }
 
-$('.skip-btn').on('click', function (){
+// $('.skip-btn').on('click', function (){
 
-})
+// })
 
-if (workRound % 4 == 0 && workTime == 0) {
-    longBreak();
-}
+// if (workRound % 4 == 0 && workTime == 0) {
+//     longBreak();
+// }
 
 
 //     SkipInterval() does the following:
