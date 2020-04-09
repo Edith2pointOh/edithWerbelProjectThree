826b1b2fd374282e7bd2e2271de8d969166e20fd
Edith Werbel <edith.werbel@gmail.com>
2020-04-09 16:17:43 -0400

    added insert video funciton

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 let counterId;
-let timerCount = 7;
+let timerCount = 25 * 60;
 let breaks = [5 * 60, 5 * 60, 5 * 60, 25 * 60];
 let breaksIndex = 0;
 let workMode = true;
@@ -12,25 +12,32 @@ $fluidEl = $('body');
 
 
 
-// const videosArr = [
-//     {
-//         exercise: "Jumping Jacks",
-//         link: <iframe width="560" height="315" src="https://www.youtube.com/embed/jOgJkRcCzUI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-//     }
-// ]
+const videosArr = [
+    {
+        exercise: "Jumping Jacks",
+        link: '<iframe src="https://www.youtube.com/embed/XocVw3xuYow" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+    },
+    {
+        exercise: "Squats",
+        link: '<iframe width="560" height="315" src="https://www.youtube.com/embed/XocVw3xuYow" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+    }
+]
 
 
 $(function (){
 
-$('.play-btn').on('click', function () {
+function togglePlayButton(){
     $('.pause-btn').toggle();
     $('.play-btn').toggle();
+    isPaused = !isPaused;
+}
+$('.play-btn').on('click', function () {
+    togglePlayButton();
     play();
 })
 
 $('.pause-btn').on('click', function() {
-    $('.pause-btn').toggle();
-    $('.play-btn').toggle();
+    togglePlayButton();
     pause();
 })
 
@@ -42,6 +49,8 @@ $('.move-skip-btns').on('click', function() {
     skip();
 })
 
+
+// CALCULATE TIME AND TIME PASSED
 function timer(func, seconds) {
     const now = Date.now();
     const then = now + (seconds * 1000);
@@ -49,9 +58,11 @@ function timer(func, seconds) {
         timerCount = Math.round((then - Date.now()) / 1000);
         func();
     }, 1000);
+    console.log(`created in timer ${id}`);
     return id;
 }
 
+// SHOW IN DIGITAL FORMAT
 function displayTimeLeft(seconds) {
     const minutes = Math.floor(seconds / 60);
     const RemainingSeconds = seconds % 60;
@@ -66,13 +77,15 @@ function displayTimeLeft(seconds) {
     }
 }
 
+// WORK SESSION FUNCTION
 function startWork() {
     workMode = true;
     isPaused = false;
     workRound++;
     switchMode();
-    timerCount = 7;
-    // timerCount = 25 * 60;
+    timerCount = 25 * 60;
+    displayTimeLeft(timerCount);
+    togglePlayButton();
     play();
 }
 
@@ -81,34 +94,52 @@ function switchMode() {
     $('.inner-container-move').toggle();
 }
 
+// BREAK/ MOVE SESSION FUNCTION
 function startBreak() {
+    insertVideo();
     workMode = false;
+    isPaused = false;
     moveRound++;
     switchMode();
-    timerCount = 5;
-    // timerCount = breaks[breaksIndex];
+    timerCount = [5 * 60, 5 * 60, 5 * 60, 25 * 60];
+    timerCount = breaks[breaksIndex];
     if (breaks.length === breaksIndex) {
         breaksIndex = 0;
     }
     else {
         breaksIndex++;
     }
+    togglePlayButton();
     play();
 }
 
+// choose random video
+// remove from original array
+// inset youtube video
+
+function insertVideo () {
+    let randomVideo = Math.floor(Math.random() * videosArr.length);
+    console.log(randomVideo);
+    $('.excercise-name').text(videosArr[randomVideo].exercise);
+    $('.exercise-wrapper').replaceWith(videosArr[randomVideo].link);
+}
+
 function pause() {
-    isPaused = true;
+    console.log(`cleared in paused ${counterId}`);
     clearInterval(counterId);
 }
 
 function play(){
     counterId = timer(function () {
         if (workRound === 12 && timerCount <= 0) {
+            console.log(`cleared in play ${counterId}`);
             clearInterval(counterId);
             $('.inner-container-work').toggle();
             $('.inner-container-end-page').toggle();
         }
         else if (timerCount <= 0 ) {
+            console.log(`cleared in timerCount ${counterId}`);
+
             clearInterval(counterId);
             $('#sound')[0].play()
             skip();
@@ -118,20 +149,22 @@ function play(){
 }
 
 function skip() {
+    console.log(`cleared in skip ${counterId}`);
+
     clearInterval(counterId);
     if (workRound === 12){
         $('.inner-container-work').toggle();
         $('.inner-container-end-page').toggle();
     }
     else if (workMode) {
-        if (isPaused){
+        if (!isPaused){
             $('.pause-btn').toggle();
             $('.play-btn').toggle(); 
         }
         startBreak();
     }
     else {
-        if (isPaused) {
+        if (!isPaused) {
             $('.pause-btn').toggle();
             $('.play-btn').toggle(); 
         }
